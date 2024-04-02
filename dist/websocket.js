2cf2e4fd2e6a4d1fbe4479ee9df40ac07d39296c
jakebuceac <jake.buceac@in2tel.ie>
2024-04-02 10:53:38 +0100

    removed dist folder

--- dist/websocket.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ws_1 = require("ws");
-const webSocketServer = new ws_1.WebSocketServer({ port: 443 });
-exports.default = webSocketServer;
